bd1655a4f93fdf1a131b070fc12a61da462d5e3b
Полина Полянская <ppolina353@yandex.ru>
2022-10-14 21:29:49 +0300

    куча всего, <> ааааа

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
 Window.size = (600, 400)  
-Window.clearcolor = (3/255, 186/255, 255/255, 1) 
+Window.clearcolor = (3/255, 6/255, 5/255, 1) 
 #Window.title = "DR1"
 
 class MyApp(App):
@@ -26,6 +26,9 @@ class MyApp(App):
         
 
     def algor(self, data):    #data это словами число
+        lev_str = '   >'
+        pra_str = '<   '
+
         sot_ar = {1: 'сто', 2: 'двести', 3: 'триста', 4: 'четыреста', 5: 'пятьсот', 6: 'шестьсот', 7: 'семьсот', 8: 'восемьсот', 9: 'девятьсот'}
         des_ar = {  2: 'двадцать', 3: 'тридцать', 4: 'сорок', 5: 'пятьдесят', 6: 'шестьдесят', 7: 'семьдесят', 8: 'восемьдесят', 9: 'девяносто'}
         ed_ar =  {  1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
@@ -49,72 +52,160 @@ class MyApp(App):
         osh = []
         if (slova[0]=='') and (slova[1]=='') and (slova[2]==''):
             osh.append('вы ничего не написали :(')
-        else:
+        elif slova[2] != '':
             if (slova[0] not in des_ar.values() and slova[0] not in onl_ar.values() and slova[0] not in ed_ar.values() and slova[0] not in sot_ar.values()):
-                osh.append('первое слово написано неправильно')
-            elif (slova[0] in ed_ar.values()):
-                osh.append('первое слово не должно быть единицами')
-            elif (slova[0] in des_ar.values()):
-                osh.append('первое слово не должно быть десятками')
-            elif (slova[0] in onl_ar.values()):
-                osh.append('первое слово не должно быть формата 10-19')
-
-            if (slova[1] not in des_ar.values() and slova[1] not in onl_ar.values() and slova[1] not in ed_ar.values() and slova[1] not in sot_ar.values() and slova[1]!=''):
-                osh.append('второе слово написано неправильно')
+                osh.append('первое слово ('+slova[0]+') написано неправильно')
+                self.input_data.text = " "+ lev_str + slova[0] + pra_str +" "+slova[1]+" "+ slova[2]     
+            elif (slova[1] not in des_ar.values() and slova[1] not in onl_ar.values() and slova[1] not in ed_ar.values() and slova[1] not in sot_ar.values() and slova[1]!=''):
+                self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]     
+                osh.append('второе слово ('+slova[1]+') написано неправильно')
             elif (slova[1] in sot_ar.values()):
-                osh.append('второе слово не должно быть сотней')
+                osh.append('второе слово ('+slova[1]+') не должно быть сотней')
+                self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]     
 
-            if (slova[2] not in ed_ar.values() and slova[2] not in des_ar.values() and slova[2] not in onl_ar.values() and slova[2] not in sot_ar.values() and slova[2]!=''):
-                osh.append('третье слово написано неправильно')
+            elif (slova[2] not in ed_ar.values() and slova[2] not in des_ar.values() and slova[2] not in onl_ar.values() and slova[2] not in sot_ar.values() and slova[2]!=''):
+                osh.append('третье слово  ('+slova[2]+') написано неправильно')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
             elif (slova[2] in sot_ar.values()):
-                osh.append('третье слово не должно быть сотней')
+                osh.append('третье слово  ('+slova[2]+') не должно быть сотней')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
            
-            if (slova[1] in ed_ar.values()) and (slova[2] in ed_ar.values()):
-                osh.append('нельзя два слова единичного формата')
+            elif (slova[1] in ed_ar.values()) and (slova[2] in ed_ar.values()):
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя два слова подряд ('+slova[1]+' '+slova[2]+') единичного формата')
             elif (slova[1] in des_ar.values()) and (slova[2] in des_ar.values()):
-                osh.append('нельзя два слова десятичного формата')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя писать два слова ('+slova[1]+' '+slova[2]+') подряд десятичного формата ')
             elif (slova[1] in onl_ar.values()) and (slova[2] in onl_ar.values()):
-                osh.append('нельзя два слова формата 10-19')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя писать два слова ('+slova[1]+' '+slova[2]+') подряд формата 10-19')
             elif (slova[1] in des_ar.values()) and (slova[2] in onl_ar.values()):
-                osh.append('нельзя сначала писать десятки, потом число формата 10-19')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя сначала писать десятки('+slova[1]+'), потом число формата 10-19('+slova[2]+')')
             elif (slova[1] in ed_ar.values()) and (slova[2] in des_ar.values()):
-                osh.append('нельзя сначала писать слово единичного формата, потом десятки')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя сначала писать слово единичного формата('+slova[1]+'), потом десятки('+slova[2]+')')
             elif (slova[1] in ed_ar.values()) and (slova[2] in onl_ar.values()):
-                osh.append('нельзя сначала писать слово единичного формата, потом слово формата 10-19')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя сначала писать слово единичного формата('+slova[1]+'), потом слово формата 10-19('+slova[2]+')')
             elif (slova[1] in ed_ar.values()) and (slova[2] in sot_ar.values()):
-                osh.append('нельзя сначала писать слово единичного формата, потом сотни')
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя сначала писать слово единичного формата('+slova[1]+'), потом сотни('+slova[2]+')')
             elif (slova[1] in onl_ar.values()) and not(slova[2] == ''):
-                osh.append('нельзя после слова форматом 10-19 писать еще слово')          
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя после слова форматом 10-19('+slova[1]+') писать еще слово('+slova[2]+')')          
             elif (slova[1] in ed_ar.values()) and not(slova[2] == ''):
-                osh.append('нельзя после слова единичного формата писать еще слово')                      
+                self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                osh.append('нельзя после слова единичного формата('+slova[1]+') писать еще слово('+slova[2]+')')                      
 
         if osh:
-            self.cifr.text = ";\n".join(osh) 
+            #self.cifr.text = ";\n".join(osh)
+            self.cifr.text = '>error<' 
         else:
             rev_sot = {v:k for k, v in sot_ar.items()}
             rev_des = {v:k for k, v in des_ar.items()}
             rev_ed = {v:k for k, v in ed_ar.items()}
             rev_onl = {v:k for k, v in onl_ar.items()}
-            s = str(rev_sot[slova[0]])
+            
+            if slova[0] not in sot_ar.values():
+                fir, sec = '', ''
+                osh1 = [] 
+                #if slova[2] != '':
+                 #   er.append('-') 
 
-            if (slova[1] in onl_ar.values()):
-                q = str(rev_onl[slova[1]])
-                self.cifr.text = s + q 
-            elif (slova[1] in ed_ar.values()):
-                q = '0' + str(rev_ed[slova[1]])
-                self.cifr.text = s + q 
-            elif (slova[1] in des_ar.values()) and (slova[2] in ed_ar.values()):
-                d = str(rev_des[slova[1]])
-                e = str(rev_ed[slova[2]])
-                self.cifr.text = s + d + e 
-            elif (slova[1] in des_ar.values()) and (slova[2] == ''):
-                d = str(rev_des[slova[1]])
-                e = '0'
-                self.cifr.text = s + d + e 
-            elif (slova[1] == '') and (slova[2] == ''):
-                d = '0'
-                e = '0'
-                self.cifr.text = s + d + e 
+                if (slova[0] not in des_ar.values() and slova[0] not in onl_ar.values() and slova[0] not in ed_ar.values() and slova[0] not in sot_ar.values() and slova[0]!=''):
+                    self.input_data.text = lev_str + slova[0] + pra_str +" "+ slova[1] +" "+ slova[2]     
+                    osh1.append('второе слово ('+slova[1]+') написано неправильно')
+                elif (slova[0] in sot_ar.values()):
+                    osh1.append('второе слово ('+slova[1]+') не должно быть сотней')
+                    self.input_data.text = lev_str + slova[0] + pra_str +" "+ slova[1] +" "+ slova[2]     
+
+                elif (slova[1] not in ed_ar.values() and slova[1] not in des_ar.values() and slova[1] not in onl_ar.values() and slova[1] not in sot_ar.values() and slova[1]!=''):
+                    osh1.append('третье слово  ('+slova[2]+') написано неправильно')
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                elif (slova[1] in sot_ar.values()):
+                    osh1.append('третье слово  ('+slova[2]+') не должно быть сотней')
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+
+                elif (slova[0] in ed_ar.values()) and (slova[1] in ed_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя два слова подряд ('+slova[1]+' '+slova[2]+') единичного формата')
+                elif (slova[0] in des_ar.values()) and (slova[1] in des_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя писать два слова ('+slova[1]+' '+slova[2]+') подряд десятичного формата ')
+                elif (slova[0] in onl_ar.values()) and (slova[1] in onl_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя писать два слова ('+slova[1]+' '+slova[2]+') подряд формата 10-19')
+                elif (slova[0] in des_ar.values()) and (slova[1] in onl_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя сначала писать десятки('+slova[1]+'), потом число формата 10-19('+slova[2]+')')
+                elif (slova[0] in ed_ar.values()) and (slova[1] in des_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя сначала писать слово единичного формата('+slova[1]+'), потом десятки('+slova[2]+')')
+                elif (slova[0] in ed_ar.values()) and (slova[1] in onl_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя сначала писать слово единичного формата('+slova[1]+'), потом слово формата 10-19('+slova[2]+')')
+                elif (slova[0] in ed_ar.values()) and (slova[1] in sot_ar.values()):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя сначала писать слово единичного формата('+slova[1]+'), потом сотни('+slova[2]+')')
+                elif (slova[0] in onl_ar.values()) and not(slova[1] == ''):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]   
+                    osh1.append('нельзя после слова форматом 10-19('+slova[1]+') писать еще слово('+slova[2]+')')          
+                elif (slova[0] in ed_ar.values()) and not(slova[1] == ''):
+                    self.input_data.text = slova[0] +" "+ lev_str + slova[1] + pra_str +" "+ slova[2]  
+                    osh1.append('нельзя после слова единичного формата('+slova[1]+') писать еще слово('+slova[2]+')')                      
+                elif slova[2]!='':
+                    self.input_data.text = slova[0]+" "+ slova[1] +" "+ lev_str + slova[2] + pra_str 
+                    osh1.append('третье')
+####
+                if osh1:
+                    self.cifr.text = '>error<'
+                else:
+                    if slova[0]=='десять' and slova[1]=='' and slova[2]=='': 
+                        fir = '10'
+                    elif (slova[0] in ed_ar.values()):
+                        fir = str(rev_ed[slova[0]])
+                    elif (slova[0] in des_ar.values() and slova[1] in ed_ar.values()):
+                        fir = str(rev_des[slova[0]])
+                        sec = str(rev_ed[slova[1]])
+                    elif (slova[0] in des_ar.values() and slova[1]==''):
+                        fir = str(rev_des[slova[0]])
+                        sec = '0'               
+                    elif (slova[0] in onl_ar.values()):
+                        fir = str(rev_onl[slova[0]])
+                    else:
+                        fir = 'ошибка'
+                    #if (slova[0] in sot_ar.values()):
+                    #   fir = str(rev_sot[slova[0]])
+
+                    
+                    self.cifr.text = fir + sec 
+            else:
+                s = str(rev_sot[slova[0]])
+
+                if (slova[1] in onl_ar.values()):   #сто тринадцать
+                    q = str(rev_onl[slova[1]])
+                    qqq = s + q
+                    #self.cifr.text = s + q 
+                    self.cifr.text = qqq 
+                    ###self.input_data.foreground_color = (9,9,9,1)
+                    #self.cifr.font_size = 78
+                    #self.cifr.text = '[color=FFFF00]+self.cifr.text[0]+[/color]'+self.cifr.text[1:]    # "[color=3333ff]proud[/color]"
+                elif (slova[1] in ed_ar.values()):
+                    q = '0' + str(rev_ed[slova[1]])
+                    self.cifr.text = s + q 
+                elif (slova[1] in des_ar.values()) and (slova[2] in ed_ar.values()):
+                    d = str(rev_des[slova[1]])
+                    e = str(rev_ed[slova[2]])
+                    self.cifr.text = s + d + e 
+                elif (slova[1] in des_ar.values()) and (slova[2] == ''):
+                    d = str(rev_des[slova[1]])
+                    e = '0'
+                    self.cifr.text = s + d + e 
+                elif (slova[1] == '') and (slova[2] == ''):
+                    d = '0'
+                    e = '0'
+                    self.cifr.text = s + d + e 
         osh = []
 
     def build(self):
